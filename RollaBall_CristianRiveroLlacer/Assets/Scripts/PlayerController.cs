4d86831d7cd519ddb88ff0892eb7253bc2b88da4
elhabana <habanaparner@gmail.com>
2025-11-07 17:10:25 +0100

    Subida de Prueba

---
+++ RollaBall_CristianRiveroLlacer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Sound Configuration")]
+    public AudioClip[] soundcollection;
+
+    [Header("Editor References")]
+    public Rigidbody playerRb;
+    public AudioSource playeraudio;
+
+    [Header("Movement Parameters")]
+    public float speed = 5;
+    public Vector2 moveInput;
+
+    [Header("Jump Parameters")]
+    public float jumpforce = 5.0f;
+    public bool isGrounded;
+
+    [Header("Respawn System")]
+    public Transform respawnPoint;
+    public float falllimit = -10;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CinematicMovement();
+        if (transform.position.y <= falllimit)
+        {
+            Respawn();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        PhysicalMovement();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        transform.position = respawnPoint.transform.position;
+        playerRb.linearVelocity = Vector3.zero;
+        PlaySFX(2);
+    }
+
+    public void PlaySFX(int soundToPlay)
+    {
+        playeraudio.PlayOneShot(soundcollection[soundToPlay]);
+    }
+
+    void CinematicMovement()
+    {
+        transform.Translate(Vector3.forward * speed * moveInput.y * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * moveInput.x * Time.deltaTime);
+    }
+    void PhysicalMovement()
+    {
+        playerRb.AddForce(Vector3.forward*speed*moveInput.y);
+        playerRb.AddForce(Vector3.right*speed*moveInput.x);
+
+    }
+    void Jump()
+    {
+        playerRb.AddForce(Vector3.up * jumpforce,ForceMode.Impulse);
+        PlaySFX(0);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput=context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded==true && context.performed)
+        { 
+            isGrounded= false;
+            Jump();
+
+        }
+
+    }
+
+}
---
+++ RollaBall_CristianRiveroLlacer/Assets/Scripts/PlayerInteractor.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerInteractor : MonoBehaviour
+{
+    public int points;
+    public int winpoints;
+    public TMP_Text pointsText;
+
+    [Header("Sound References")]
+    public PlayerController playerCont;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        points = 0;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (points >= winpoints) 
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        pointsText.text = "Points: " + points.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            points++;
+
+            // Esta Opcion Consume mas RAM,
+            // Destroy(other.gameObject);
+
+            other.gameObject.SetActive(false);
+            playerCont.PlaySFX(1);
+        }
+    }
+}
