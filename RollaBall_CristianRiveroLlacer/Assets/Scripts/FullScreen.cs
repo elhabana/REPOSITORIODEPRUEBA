99f7f62d6ba9b7332a8a0e7abcfab922bb612101
elhabana <habanaparner@gmail.com>
2025-11-10 00:55:12 +0100

    He añadido la función de pantalla completa
    El menú de opciones estaba muy vacío y he puesto un toggle que activa/desactiva la pantalla completa.

---
+++ RollaBall_CristianRiveroLlacer/Assets/Scripts/FullScreen.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FullScreen : MonoBehaviour
+{
+    public Toggle toggle;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        if (Screen.fullScreen) 
+        {
+            toggle.isOn = true;        
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivaPantallaCompleta(bool pantallaCompleta)
+    {
+        Screen.fullScreen = pantallaCompleta;
+    }
+}
