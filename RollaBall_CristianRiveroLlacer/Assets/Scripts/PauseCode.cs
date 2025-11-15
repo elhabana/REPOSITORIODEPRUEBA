0a7bec5104416239f43a94cfd07d2648f273f164
elhabana <habanaparner@gmail.com>
2025-11-15 16:51:06 +0100

    una linia de codigo tontorrona

--- RollaBall_CristianRiveroLlacer/Assets/Scripts/PauseCode.cs
+++ RollaBall_CristianRiveroLlacer/Assets/Scripts/PauseCode.cs
@@ -13,6 +13,7 @@ public class PauseCode : MonoBehaviour
     {
         objectpause.SetActive(false);
         Time.timeScale = 1;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
