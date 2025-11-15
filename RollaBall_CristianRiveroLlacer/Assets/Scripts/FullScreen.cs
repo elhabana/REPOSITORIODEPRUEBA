baf4d6b5d4071f29436546e429726024010a2e09
elhabana <habanaparner@gmail.com>
2025-11-15 02:03:48 +0100

    Revert "arreglillo"
    This reverts commit 762336311ebd271d486c83ecd4551f9455cade40.

--- RollaBall_CristianRiveroLlacer/Assets/Scripts/FullScreen.cs
+++ RollaBall_CristianRiveroLlacer/Assets/Scripts/FullScreen.cs
@@ -37,4 +37,41 @@ public class FullScreen : MonoBehaviour
     {
         Screen.fullScreen = fullScreen;
     }
+<<<<<<< HEAD
+=======
+
+    public void ReviewResolution()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolution = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (Screen.fullScreen && resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolution = i;
+            }
+        }
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolution;
+        resolutionDropDown.RefreshShownValue();
+
+        resolutionDropDown.value = PlayerPrefs.GetInt("numberResolution", 0);
+
+    }
+
+    public void ChangeResolution(int resolutionIndex)
+    {
+        PlayerPrefs.SetInt("numberResolution", resolutionDropDown.value);
+
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+>>>>>>> parent of 26472b0 (arreglito)
 }
